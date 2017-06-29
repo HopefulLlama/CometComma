839ce9cc0dcf173d4a714ecfd1a59e77e1cd036c
Jonathan Law <jl185305@ncr.com>
2017-06-29 15:49:57 +0100

    Wow got one page working woo

--- client/main.js
+++ client/main.js
@@ -1,22 +1,35 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import angularRouter from 'angular-ui-router';
 
 import './main.html';
+import home from '../templates/home.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+export default angular.module('CommaBeanApp', [
+	angularMeteor,
+	angularRouter
+])
+.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/');
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+	$stateProvider
+	.state('home', {
+		url: '/',
+		templateUrl: home,
+		controller: 'HomeCtrl'
+	})
+})
+.controller('HomeCtrl', ['$scope', function($scope) {
+	function Thing(css, header, body) {
+		this.css = css;
+		this.header = header;
+		this.body = body;
+	}
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+	$scope.things = [
+		new Thing('euro', 'European', 'I am a classy French dog! I don\'t think I know how to speak any French though.'),
+		new Thing('resize-small', 'Small', 'I am the runt of my litter. I am the size of two bags of sugar, and I weigh about that too.'),
+		new Thing('queen', 'Pineapple Hair', 'I wear my hair in a bobble on the top of my head, like a pineapple. I do this because I am a queen and my bobble is my crown.'),
+		new Thing('baby-formula', 'No Teeth', 'I lost all my teeth. I can only eat small bits of food, or soft food. It\'s hard to keep my tongue in my mouth, so I let it hang a lot.')
+	];
+}]);
